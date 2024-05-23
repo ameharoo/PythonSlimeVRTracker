Fix send rotation packet.\nSeparate methods prepage and do.\n Rename quaternion_from_vectors to find_quaternion_of_rotation

slimevr/async_tracker_emulator.py
@@ -4,8 +4,10 @@ import math
 from socket import AF_INET, SOCK_DGRAM, socket
 import time
 
+import numpy as np
+
 from slimevr.tracker_packet import DatagramPacket
-from slimevr.types import ImuType, PacketType, SensorAcceleration, SensorData, SensorRotationQuat, SensorState
+from slimevr.tracker_types import Helper, ImuType, PacketType, SensorAcceleration, SensorData, SensorPosition, SensorRotationQuat, SensorState
 
 
 class SlimeVRTrackerEmulator():
@@ -27,6 +29,10 @@ class SlimeVRTrackerEmulator():
 
         self.sock = None
 
+    async def get_trackers(self) -> list[SensorData]:
+        async with self._sensors_lock:
+            return list(self._sensors.values())
+
     async def set_tracker(self, tracker: SensorData):
         async with self._sensors_lock:
             self._sensors[tracker.id] = tracker
@@ -38,7 +44,24 @@ class SlimeVRTrackerEmulator():
     async def set_tracker_rotation(self, id: int, rotation: SensorRotationQuat):
         async with self._sensors_lock:
             self._sensors[id].rotation = rotation
+            
 
+    async def set_tracker_position(self, id: int, position: SensorPosition):
+        async with self._sensors_lock:
+            # Set new position
+            # position = SensorPosition(position.x, 0, -position.y)
+
+            A = np.array([self._sensors[id].position.x, self._sensors[id].position.y, self._sensors[id].position.z])
+            B = np.array([position.x, position.y, position.z])
+            
+            # Get rotation for rotate A to B
+            quat = Helper.find_quaternion_of_rotation(A, B)
+            
+            # Set rotation and acceleration
+            self._sensors[id].rotation = SensorRotationQuat(quat[0], quat[1], quat[2], quat[3])
+            self._sensors[id].acceleration = SensorAcceleration(position.x * 1000, position.y * 1000, position.z * 1000)
+
+            # self._sensors[id].position = position
 
     async def get_packet_number(self) -> int:
         async with self._packet_number_lock:
@@ -59,7 +82,7 @@ class SlimeVRTrackerEmulator():
     async def send_sensor_rotation(self, sock: socket, remote_addr, sensor: SensorData):
         loop = asyncio.get_event_loop()
 
-        # sensor.rotation = SensorRotationQuat.from_euler(-math.pi/2, 0, 0)
+        #sensor.rotation = SensorRotationQuat(math.sin(time.time()),2,3,1)
 
         pb = DatagramPacket()
         pb.send_sensor_rotation(await self.get_packet_number(), 
@@ -76,12 +99,14 @@ class SlimeVRTrackerEmulator():
     async def send_sensor_acceleration(self, sock: socket, remote_addr, sensor: SensorData):
         loop = asyncio.get_event_loop()
 
+        # sensor.acceleration = SensorAcceleration(0,0,0)
+
         pb = DatagramPacket()
         pb.send_sensor_acceleration(await self.get_packet_number(), 
                                     sensor.id, 
-                                    sensor.acceleration.x * 100, 
-                                    sensor.acceleration.y * 100,
-                                    sensor.acceleration.z * 100)
+                                    sensor.acceleration.x, 
+                                    sensor.acceleration.y,
+                                    sensor.acceleration.z)
         
         # await asyncio.sleep(0.01)
         await loop.sock_sendto(sock, pb.buf, remote_addr)
@@ -173,16 +198,15 @@ class SlimeVRTrackerEmulator():
         await self.send_trackers_data(sock, remote_addr)
         await self.send_feature_flags(sock, remote_addr)
     
-    # async def do(self, addr):
-    #     loop = asyncio.get_event_loop()
+    async def do(self, addr):
+        loop = asyncio.get_event_loop()
 
-    #     await self.send_trackers_data(self.sock, addr)
+        await self.send_trackers_data(self.sock, addr)
 
-    #     recv_buf, remote_addr = await loop.sock_recvfrom(self.sock, 1024)
-    #     await self.handle_packet(self.sock, remote_addr, recv_buf)
+        recv_buf, remote_addr = await loop.sock_recvfrom(self.sock, 1024)
+        await self.handle_packet(self.sock, remote_addr, recv_buf)
 
-
-    async def run(self, addr):
+    async def prepare(self, addr):
         loop = asyncio.get_event_loop()
 
         self.sock = socket(AF_INET, SOCK_DGRAM)
@@ -190,9 +214,10 @@ class SlimeVRTrackerEmulator():
 
         await self.send_tracker_discovery_packet(self.sock, addr)
 
-        while True:
-            self.send_trackers_data(self.sock, addr)
 
-            recv_buf, remote_addr = await loop.sock_recvfrom(self.sock, 1024)
-            await self.handle_packet(self.sock, remote_addr, recv_buf)
+    async def run(self, addr):
+        await self.prepare(addr)
+
+        while True:
+            await self.do(addr)
 